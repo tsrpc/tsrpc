29e6dc08e5ef1ba8582bb67bcdd3fa9670f66472
k8w <me@k8w.cn>
2026-02-27 03:16:04 +0800

    chore: remove unused _transport

--- packages/core/src/_transport/ClientTransport.ts
+++
@@ -1,6 +0,0 @@
-// export interface ClientTransport {
-//   connect: () => Promise<void>;
-//   disconnect: () => Promise<void>;
-//   sendData: (data: any) => Promise<void>;
-// }
-// export type ClientTransportFactory = (options: { onData: Function }) => ClientTransport;
